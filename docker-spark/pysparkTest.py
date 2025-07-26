7b25e1bb0c9f2b629821c8392e70279ec084c130
Immaculate <manjory.sjsu@gmail.com>
2025-07-26 10:51:51 -0700

    hello world word count on docker-spark, output in opt/spark-data/output.txt

--- docker-spark/pysparkTest.py
+++ docker-spark/pysparkTest.py
@@ -1,11 +1,25 @@
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.appName("ManjoryTestCluster").master("spark://localhost:7077").config("spark.executor.memory","1g").config("spark.executor.cores","1").getOrCreate()
+# spark = SparkSession.builder.appName("ManjoryTestCluster").master("spark://localhost:7077").config("spark.executor.memory","1g").config("spark.executor.cores","1").getOrCreate()
+#
+# print("Spark Version::",spark.version)
+#
+# df = spark.createDataFrame([(1, "apple"), (2, "banana"), (3, "cherry")], ["id", "fruit"])
+# df.show()
+#
+# spark.stop()
 
-print("Spark Version::",spark.version)
 
-df = spark.createDataFrame([(1, "apple"), (2, "banana"), (3, "cherry")], ["id", "fruit"])
-df.show()
+if __name__=="__main__":
+    spark=SparkSession.builder.appName("WordCount").getOrCreate()
+    sc=spark.sparkContext
 
-spark.stop()
+    lines=sc.textFile("/Users/macbookpro_2015/dev/python/learningSpark/docker-spark/hello-world.txt")
 
+    counts=(
+        lines.flatMap(lambda line: line.split())
+    .map(lambda word: (word,1))
+    .reduceByKey(lambda a,b: a+b)
+    )
+    counts.saveAsTextFile("/Users/macbookpro_2015/dev/python/learningSpark/docker-spark/opt/spark-data/output.txt")
+    spark.stop()
